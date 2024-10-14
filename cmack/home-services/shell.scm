e2255f91764e4ed0f5127bfa631a2db9f1e720f1
Charlie McMackin <charlie.mac@gmail.com>
2024-10-14 14:39:42 -0500

    Make custom PS1 prompt actually show
    home-environment-variables is apparently the wrong place to modify the prompt.

--- cmack/home-services/shell.scm
+++ cmack/home-services/shell.scm
@@ -1,16 +1,20 @@
 (define-module (cmack home-services shell)
   #:use-module (gnu services)
   #:use-module (gnu home services shells)
-  #:use-module (gnu home services))
+  #:use-module (gnu home services)
+  #:use-module (guix gexp))
 
 (define (home-desktop-environment-variables config)
-  '(("PS1" . "\\n\\[\\e[1;2m\\]Time: \\[\\e[0;2m\\]\\D{%F %T}\\[\\e[0;1m\\] User: \\[\\e[0;3m\\]\\u\\[\\e[0m\\]@\\[\\e[3m\\]\\h\\[\\e[0;1m\\]λ \\nDir: \\[\\e[0m\\]\\w\\$ ")))
+  (home-bash-extension
+   (bashrc
+    (list (plain-file "bash-fancy-prompt"
+                      (string-append "PS1='\\n\\[\\e[1;2m\\]Time: \\[\\e[0;2m\\]\\D{%F %T}\\[\\e[0;1m\\] User: \\[\\e[0;3m\\]\\u\\[\\e[0m\\]@\\[\\e[3m\\]\\h\\[\\e[0;1m\\] λ\\nDir: \\[\\e[0m\\]\\w${GUIX_ENVIRONMENT:+ [env]}\\$ '"))))))
 
 (define-public bash-fancy-prompt-service-type
   (service-type (name 'bash-fancy-prompt)
                 (description "My shell prompt service")
                 (extensions
                  (list (service-extension
-                        home-environment-variables-service-type
+                        home-bash-service-type
                         home-desktop-environment-variables)))
                 (default-value #f)))
